6b02d52f10b2b0326821b4fbc2423e3470c8d41f
JC Narasimhan <jcnarasimhan@google.com>
2021-08-31 09:48:09 -0700

    ansible user creation & ssh equivlence set up for new user created

--- host-provision.sh
+++ host-provision.sh
@@ -30,34 +30,13 @@ fi
 # set -x
 GETOPT_MANDATORY="inventory-file:"
 # GETOPT_OPTIONAL="ora-role-separation:,ora-disk-mgmt:,ora-swlib-path:,ora-staging:,ora-asm-disks:,ora-data-mounts:,help"
-GETOPT_OPTIONAL="help"
+GETOPT_OPTIONAL="hostprovision-ansible-user:,help"
 GETOPT_LONG="${GETOPT_MANDATORY},${GETOPT_OPTIONAL}"
 GETOPT_SHORT="h"
 
-# YESIAMSURE=0
-# VALIDATE=0
+
 INVENTORY_FILE="${INVENTORY_FILE:-./inventory_files/inventory}"
 
-# ORA_VERSION="${ORA_VERSION}"
-# ORA_VERSION_PARAM='^(19\.3\.0\.0\.0|18\.0\.0\.0\.0|12\.2\.0\.1\.0|12\.1\.0\.2\.0|11\.2\.0\.4\.0|ALL)$'
-
-# ORA_ROLE_SEPARATION="${ORA_ROLE_SEPARATION:-TRUE}"
-# ORA_ROLE_SEPARATION_PARAM="^(TRUE|FALSE)$"
-
-# ORA_DISK_MGMT="${ORA_DISK_MGMT:-UDEV}"
-# ORA_DISK_MGMT_PARAM="ASMLIB|UDEV"
-
-# ORA_SWLIB_PATH="${ORA_SWLIB_PATH:-/u01/swlib}"
-# ORA_SWLIB_PATH_PARAM="^/.*"
-
-# ORA_STAGING="${ORA_STAGING:-""}"
-# ORA_STAGING_PARAM="^/.+$"
-
-# ORA_ASM_DISKS="${ORA_ASM_DISKS:-asm_disk_config.json}"
-# ORA_ASM_DISKS_PARAM="^.+\.json$"
-
-# ORA_DATA_MOUNTS="${ORA_DATA_MOUNTS:-data_mounts_config.json}"
-# ORA_DATA_MOUNTS_PARAM="^.+\.json$"
 
 options=$(getopt --longoptions "$GETOPT_LONG" --options "$GETOPT_SHORT" -- "$@")
 
@@ -66,7 +45,7 @@ options=$(getopt --longoptions "$GETOPT_LONG" --options "$GETOPT_SHORT" -- "$@")
        exit 1
 }
 
-echo "PARSED COMMAND LINE FLAGS: $options"
+# echo "PARSED COMMAND LINE FLAGS: $options"
 
 eval set -- "$options"
 
@@ -74,35 +53,12 @@ while true; do
     case "$1" in
     --inventory-file)
         INVENTORY_FILE="$2"
-        echo "b4 shift in case inventory-file) $@"
-        shift 
-        echo "after shift in case inventory-file) $@"
-        # exit
+        shift;
         ;;
-#     --ora-role-separation)
-#         ORA_ROLE_SEPARATION="$2"
-#         shift;
-#         ;;
-#     --ora-swlib-path)
-#         ORA_SWLIB_PATH="$2"
-#         shift;
-#         ;;
-#     --ora-staging)
-#         ORA_STAGING="$2"
-#         shift;
-#         ;;
-#     --ora-disk-mgmt)
-#         ORA_DISK_MGMT="$2"
-#         shift;
-#         ;;
-#     --ora-asm-disks)
-#         ORA_ASM_DISKS="$2"
-#         shift;
-#         ;;
-#     --ora-data-mounts)
-#         ORA_DATA_MOUNTS="$2"
-#         shift;
-#         ;;
+    --hostprovision-ansible-user)
+        ORA_HOSTPROVISION_ANSIBLE_USER="$2"
+        shift;
+        ;;
     --help|-h)
         echo -e "\tUsage: `basename $0` "
         echo $GETOPT_MANDATORY|sed 's/,/\n/g'|sed 's/:/ <value>/'|sed 's/\(.\+\)/\t  --\1/'
@@ -110,7 +66,6 @@ while true; do
         exit 2
         ;;
     --)
-        echo "in case --)"
         shift
         break
         ;;
@@ -118,18 +73,6 @@ while true; do
     shift
 done
 
-# set +x
-# [[ "$ORA_STAGING" == "" ]] && {
-#     ORA_STAGING=$ORA_SWLIB_PATH
-# }
-
-# shopt -s nocasematch
-
-# # Mandatory options
-# if [ "${ORA_VERSION}" = "" ]; then
-#     echo "Please specify the oracle release with --ora-version"
-#     exit 2
-# fi
 
 if [[ ! -s ${INVENTORY_FILE} ]]; then
     echo "Please specify the inventory file using --inventory-file <file_name>"
@@ -137,51 +80,12 @@ if [[ ! -s ${INVENTORY_FILE} ]]; then
 fi
 
 
-# [[ ! "$ORA_VERSION" =~ $ORA_VERSION_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-version: $ORA_VERSION"
-#     exit 1
-# }
-# [[ ! "$ORA_ROLE_SEPARATION" =~ $ORA_ROLE_SEPARATION_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-role-separation: $ORA_ROLE_SEPARATION"
-#     exit 1
-# }
-# [[ ! "$ORA_SWLIB_PATH" =~ $ORA_SWLIB_PATH_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-swlib-path: $ORA_SWLIB_PATH"
-#     exit 1
-# }
-# [[ ! "$ORA_STAGING" =~ $ORA_STAGING_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-staging: $ORA_STAGING"
-#     exit 1
-# }
-# [[ ! "$ORA_DISK_MGMT" =~ $ORA_DISK_MGMT_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-disk-mgmt: $ORA_DISK_MGMT"
-#     exit 1
-# }
-# [[ ! "$ORA_ASM_DISKS" =~ $ORA_ASM_DISKS_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-asm-disks: $ORA_ASM_DISKS"
-#     exit 1
-# }
-# [[ ! "$ORA_DATA_MOUNTS" =~ $ORA_DATA_MOUNTS_PARAM ]] && {
-#     echo "Incorrect parameter provided for ora-data-mounts: $ORA_DATA_MOUNTS"
-#     exit 1
-# }
-
-# ORA_STAGING=${ORA_STAGING%/}
-# ORA_SWLIB_PATH=${ORA_SWLIB_PATH%/}
-
-# export ORA_ROLE_SEPARATION
-# export ORA_VERSION
-# export ORA_DISK_MGMT
-# export ORA_ASM_DISKS
-# export ORA_DATA_MOUNTS
-# export ORA_STAGING
-# export ORA_SWLIB_PATH
+export ORA_HOSTPROVISION_ANSIBLE_USER
 
 echo -e "Running with parameters from command line or environment variables:\n"
 set | egrep '^(ORA_|INVENTORY_)' | grep -v '_PARAM='
 echo
 
-# INVENTORY_FILE="localhost,"
 ANSIBLE_PARAMS="-i ${INVENTORY_FILE} ${ANSIBLE_PARAMS}"
 ANSIBLE_EXTRA_PARAMS="${*}"
 
